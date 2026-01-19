de7016943aff08b1b15dbb717847b5b34e1227d4
Gyuri Park <rbfl6418@gmail.com>
2026-01-19 18:32:23 +1000

    Extract logic as Controller of MVC

--- controller/tweet.js
+++ controller/tweet.js
@@ -1,1 +1,54 @@
 // Controller(logic)
+
+// Modle(data)
+import * as tweetRepository from "../data/tweet.js";
+
+export function getTweets(req, res) {
+  // ?? 일반 function이라서 => 쓰면 X
+  const username = req.query.username;
+  const data = username
+    ? tweetRepository.getAllByUsername(username)
+    : tweetRepository.getAll; // array
+  console.log(data);
+
+  res.status(200).json(data);
+}
+
+export function getTweet(req, res) {
+  const id = req.params.id; // string -> int
+
+  const tweet = tweetRepository.getById;
+
+  if (tweet) {
+    res.status(200).json(tweet);
+  } else {
+    res.status(404).json({ message: `Tweet id(${id}) is not found` }); // JS객체 -> json데이터로 변환해서 보냄
+  }
+}
+export function createTweet(req, res) {
+  const { text, name, username } = req.body; // object Destructuring
+  // 객체 안에 있는 값을 쏙쏙 뽑아서 변수로 만드는 문법
+
+  const tweet = tweetRepository.create(text, name, username);
+  res.status(201).json(tweet);
+}
+export function updateTweet(req, res) {
+  // req
+  const id = req.params.id;
+  const text = req.body.text;
+  const tweet = tweetRepository.update(id, text);
+
+  if (tweet) {
+    res.status(200).json(tweet);
+  } else {
+    // tweet : undefined
+    res.status(404).json({ message: `Tweet id(${id}) is not found.` });
+  }
+}
+export function deleteTweet(req, res) {
+  const id = req.params.id;
+
+  tweetRepository.remove(id);
+
+  res.status(204).send("Succesfully deleted!");
+}
--- router/tweets.js
+++ router/tweets.js
@@ -3,62 +3,19 @@
 import express from "express";
 const app = express();
 const router = express.Router();
-// data
-import * as tweetRepository from "../data/tweet.js";
-// logic
+// Controller(logic)
 import * as controller from "../controller/tweet.js";
 
 app.use(express.json());
 
+// ?? controller.getTweets() 이렇게 ()괄호 쓰면 함수 결과값 전달하는 것 되어버림 -> getTweets 이렇게 써야함
 router
-  .get("/", (req, res) => {
-    const username = req.query.username;
-    const data = username
-      ? tweetRepository.getAllByUsername(username)
-      : tweetRepository.getAll; // array
-    console.log(data);
-
-    res.status(200).json(data);
-  })
-  .post("/", (req, res) => {
-    const { text, name, username } = req.body; // object Destructuring
-    // 객체 안에 있는 값을 쏙쏙 뽑아서 변수로 만드는 문법
-
-    const tweet = tweetRepository.create(text, name, username);
-    res.status(201).json(tweet);
-  });
+  .get("/", controller.getTweets) //
+  .post("/", controller.createTweet);
 
 router
-  .get("/:id", (req, res) => {
-    const id = req.params.id; // string -> int
-
-    const tweet = tweetRepository.getById;
-
-    if (tweet) {
-      res.status(200).json(tweet);
-    } else {
-      res.status(404).json({ message: `Tweet id(${id}) is not found` }); // JS객체 -> json데이터로 변환해서 보냄
-    }
-  })
-  .put("/:id", (req, res) => {
-    // req
-    const id = req.params.id;
-    const text = req.body.text;
-    const tweet = tweetRepository.update(id, text);
-
-    if (tweet) {
-      res.status(200).json(tweet);
-    } else {
-      // tweet : undefined
-      res.status(404).json({ message: `Tweet id(${id}) is not found.` });
-    }
-  })
-  .delete("/:id", (req, res) => {
-    const id = req.params.id;
-
-    tweetRepository.remove(id);
-
-    res.status(204).send("Succesfully deleted!");
-  });
+  .get("/:id", controller.getTweet)
+  .put("/:id", controller.updateTweet)
+  .delete("/:id", controller.deleteTweet);
 
 export default router;
